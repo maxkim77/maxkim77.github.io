af4dc30f5dcb1aa0c8790a4cd1da076ed7354d06
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2024-07-16 17:17:33 +0900

    :sparkles: update
    new post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,19 @@
 var store = [{
         "title": "첫 제목",
-        "excerpt":"오늘 처음 블로그   심   @            @  ","categories": [],
-        "tags": [],
-        "url": "/first/",
+        "excerpt":"오늘 처음 블로그   심   @            @  ","categories": ["coding"],
+        "tags": ["python","blog","jekyll"],
+        "url": "/coding/first/",
+        "teaser": null
+      },{
+        "title": "[CS] Understanding Packet Switching: Virtual Circuit and Datagram Methods with Quiz",
+        "excerpt":"What term describes the number of unique values in a specific dataset? Explanation: Cardinality refers to the distinct count of values within a dataset, indicating how many unique values exist. What term is used to describe the actual set of data stored in a database table? Explanation: A relation instance...","categories": ["CS"],
+        "tags": ["CS","Knowledge","Quiz","Network","Packet Switching"],
+        "url": "/cs/CSQuiz1/",
+        "teaser": null
+      },{
+        "title": "[CS] Frequently Used SQL syntax",
+        "excerpt":"Q. What SQL statement is used to retrieve data from a database? A. The SELECT statement is used to fetch data from one or more tables in a database. SELECT * FROM table_name; Q. How do you insert new data into a table named customers with columns name and email?...","categories": ["CS"],
+        "tags": ["CS","Knowledge","Quiz","SQL"],
+        "url": "/cs/SQL/",
         "teaser": null
       }]
